c690c8334b9720ca475557d52235952d2451902f
thanaanaht <160087095+thanaanaht@users.noreply.github.com>
2024-02-15 14:33:33 +0700

    Update HTML File
    Raksupercar v2

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const chalk = require('chalk');
 const debug = require('debug')('app');
+const path = require('path');
 
 const morgan = require('morgan');
 
 const app = express();
 const port = 3000;
-
+app.use(morgan('combined'));
+app.use(express.static(path.join(__dirname,"/public/")))
 app.get("/", (_req, res) => {
     res.send('Hello Nui Thanakorn');
 });
