d28b63a45ce556b3685842690506b7dd2866309b
thanaanaht <160087095+thanaanaht@users.noreply.github.com>
2024-02-15 09:13:10 +0700

    Add all files of Project
    Hello world 100%

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const chalk = require('chalk');
+const debug = require('debug')('app');
+
+const morgan = require('morgan');
+
+const app = express();
+const port = 3000;
+
+app.get("/", (_req, res) => {
+    res.send('Hello Nui Thanakorn');
+});
+
+app.listen(port, () => {
+    debug("Listening on port %d" + chalk.green(port));
+});
